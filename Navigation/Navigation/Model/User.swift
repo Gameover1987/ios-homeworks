80dfd0049d82afc746154a43a3cd3e1e7a84db9a
Vyacheslav Nekrasov <slnec@yandex.ru>
2022-09-25 18:43:01 +0700

    Task 2 - TestuserService with test user

--- Navigation/Navigation/Model/User.swift
+++ Navigation/Navigation/Model/User.swift
@@ -11,9 +11,10 @@ public class User {
     
     public var status : String = ""
     
-    init(login : String, avatar : UIImage) {
+    init(login : String, avatar : UIImage, status: String) {
         self.login = login
         self.avatar = avatar
+        self.status = status
     }
 }
 
@@ -22,7 +23,7 @@ public protocol UserService {
 }
 
 public class CurrentUserService : UserService {
-    private let user : User = User(login: "Gamever", avatar: UIImage(named: "Ava")!)
+    private let user : User = User(login: "Gamever", avatar: UIImage(named: "Ava")!,status: "Release user")
     
     public func getUser(login: String) -> User? {
         if (login == user.login) {
@@ -31,4 +32,31 @@ public class CurrentUserService : UserService {
         
         return nil
     }
+}
+
+public class TestUserService : UserService {
+    
+    private let user : User = User(login: "Gamever", avatar: getBlurImage(), status: "Test user")
+    
+    public func getUser(login: String) -> User? {
+        if (login == user.login) {
+            return user
+        }
+        
+        return nil
+    }
+    
+    private static func getBlurImage() -> UIImage {
+        let image = UIImage(named: "Ava")!
+        
+        let inputImage = CIImage(cgImage: image.cgImage!)
+        
+        let filter = CIFilter(name: "CIGaussianBlur")
+        filter?.setValue(inputImage, forKey: "inputImage")
+        filter?.setValue(10, forKey: "inputRadius")
+        let blurred = filter?.outputImage
+        let blurImage = UIImage(ciImage: blurred!)
+        return blurImage
+    }
+    
 }
