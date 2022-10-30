c495fe19e06cebdb8f4af19589b47cf940af230e
Vyacheslav Nekrasov <slnec@yandex.ru>
2022-10-31 01:07:34 +0700

    Initial commit refactorings

--- Navigation/Navigation/Model/Photo.swift
+++ Navigation/Navigation/Model/Photo.swift
@@ -9,46 +9,4 @@ final class Photo {
     }
 }
 
-final class PhotoStorage {
-    
-    public let photos: [Photo] = [Photo("photo1"),
-                           Photo("photo2"),
-                           Photo("photo3"),
-                           Photo("photo4"),
-                           Photo("photo5"),
-                           Photo("photo6"),
-                           Photo("photo7"),
-                           Photo("photo8"),
-                           Photo("photo9"),
-                           Photo("photo10"),
-                           Photo("photo11"),
-                           Photo("photo12"),
-                           Photo("photo13"),
-                           Photo("photo14"),
-                           Photo("photo15"),
-                           Photo("photo16"),
-                           Photo("photo17"),
-                           Photo("photo18"),
-                           Photo("photo19"),
-                           Photo("photo20")]
-    
-    public static let instance: PhotoStorage = .init()
-    
-    private init() {
-//        let fileManager = FileManager.default
-//        let path = Bundle.main.resourceURL!
-//        let assetURL = path.appendingPathComponent("RockBands.bundle")
-//        
-//        do {
-//            let contents = try fileManager.contentsOfDirectory(at: assetURL, includingPropertiesForKeys: [URLResourceKey.nameKey, URLResourceKey.isDirectoryKey], options: .includesDirectoriesPostOrder)
-//            
-//            for item in contents
-//            {
-//                photos.append(Photo(  item.lastPathComponent))
-//            }
-//        }
-//        catch let error as NSError {
-//            print(error)
-//        }
-    }
-}
+  
--- Navigation/Navigation/Model/User.swift
+++ Navigation/Navigation/Model/User.swift
@@ -18,45 +18,8 @@ public class User {
     }
 }
 
-public protocol UserService {
-    func getUser(login : String) -> User?
-}
 
-public class CurrentUserService : UserService {
-    private let user : User = User(login: "Gamever", avatar: UIImage(named: "Ava")!,status: "Release user")
-    
-    public func getUser(login: String) -> User? {
-        if (login == user.login) {
-            return user
-        }
-        
-        return nil
-    }
-}
 
-public class TestUserService : UserService {
-    
-    private let user : User = User(login: "Gamever", avatar: getBlurImage(), status: "Test user")
-    
-    public func getUser(login: String) -> User? {
-        if (login == user.login) {
-            return user
-        }
-        
-        return nil
-    }
-    
-    private static func getBlurImage() -> UIImage {
-        let image = UIImage(named: "Ava")!
-        
-        let inputImage = CIImage(cgImage: image.cgImage!)
-        
-        let filter = CIFilter(name: "CIGaussianBlur")
-        filter?.setValue(inputImage, forKey: "inputImage")
-        filter?.setValue(10, forKey: "inputRadius")
-        let blurred = filter?.outputImage
-        let blurImage = UIImage(ciImage: blurred!)
-        return blurImage
-    }
-    
-}
+
+
+
