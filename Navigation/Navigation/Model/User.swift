0e42267d50d6f2aa0b6bcc0dd05c8214d3aad6d1
Gameover1987 <slnec@yandex.ru>
2023-01-08 03:40:09 +0700

    Добавил шифрование пр использовании Realm

--- Navigation/Navigation/Model/User.swift
+++ Navigation/Navigation/Model/User.swift
@@ -1,5 +1,4 @@
 
-import Foundation
 import RealmSwift
 
 class User : Object {
@@ -18,50 +17,4 @@ class User : Object {
     }
 }
 
-protocol UserProviderProtocol {
-    
-    func getStoredUser() -> User?
-    
-    func storeUser(user: User)
-}
 
-
-final class UserProvider : UserProviderProtocol {
-    
-    public static let shared = UserProvider()
-    
-    private init () {
-        let config = Realm.Configuration(schemaVersion: 1)
-        Realm.Configuration.defaultConfiguration = config
-    }
-    
-    func getStoredUser() -> User? {
-        do {
-            let realm = try Realm()
-            return realm.objects(User.self).first
-        }
-        catch {
-            print(error)
-            return nil
-        }
-    }
-    
-    func storeUser(user: User) {
-        do {
-            let realm = try Realm()
-            if realm.objects(User.self).contains(where: {storedUser in
-                return storedUser.login == user.login &&
-                       storedUser.password == user.password
-            }) {
-                return
-            }
-                
-            try realm.write {
-                realm.add(user)
-            }
-        }
-        catch {
-            print(error)
-        }
-    }
-}
