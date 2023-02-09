cf5833c2f33d3a34b3d7217ce2e7ff80ef669d79
Gameover1987 <slnec@yandex.ru>
2023-02-10 02:20:22 +0700

    Task 2*

--- Navigation/Navigation/Notifications/LocalNotificationsService.swift
+++ Navigation/Navigation/Notifications/LocalNotificationsService.swift
@@ -2,7 +2,14 @@
 import Foundation
 import UserNotifications
 
-final class LocalNotificationsService {
+final class LocalNotificationsService : NSObject {
+    
+    enum UpdateType : String {
+        case light = "Light"
+        case hard = "Hard"
+    }
+    
+    private let notificationsCategory = "achtung!"
     
     private let updatesNotificationId = "updates"
     
@@ -10,8 +17,8 @@ final class LocalNotificationsService {
     
     private let center = UNUserNotificationCenter.current()
     
-    private init () {
-        
+    private override init () {
+        super.init()
     }
     
     func registerForLatestUpdatesIfPossible() {
@@ -25,6 +32,8 @@ final class LocalNotificationsService {
             if success {
                 print("Notifications enabled by user")
                 
+                self.center.delegate = self
+                self.registerCategories()
                 self.sendUpdatesNotification()
             } else {
                 print("Notifications disabled by user")
@@ -36,16 +45,42 @@ final class LocalNotificationsService {
         let content = UNMutableNotificationContent()
         content.title = "Новая версия приложения"
         content.body = "Айда обновляться, по красоте все будет!"
-        content.categoryIdentifier = "achtung!"
+        content.categoryIdentifier = notificationsCategory
         
         var dateComponents = DateComponents()
         dateComponents.hour = 2
-        dateComponents.minute = 2
+        dateComponents.minute = 20
         
         let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
         
         let request = UNNotificationRequest(identifier: updatesNotificationId, content: content, trigger: trigger)
         
         center.add(request)
+    }
+    
+    private func registerCategories() {
+        let center = UNUserNotificationCenter.current()
+        
+        let action1 = UNNotificationAction(identifier: UpdateType.light.rawValue, title: "Обновить только данные")
+        let action2 = UNNotificationAction(identifier: UpdateType.hard.rawValue, title: "Обновить приложение целиком")
+        
+        let category = UNNotificationCategory(identifier: notificationsCategory, actions: [action1, action2], intentIdentifiers: [])
+        
+        center.setNotificationCategories([category])
+    }
+}
+
+extension LocalNotificationsService : UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        switch response.actionIdentifier {
+        case UpdateType.light.rawValue:
+            print("Будут обновлены данные приложения")
+        case UpdateType.hard.rawValue:
+            print("Приложение будет обновлено целиком")
+        default:
+            print("Неизвестный тип действия по уведомлению!")
+        }
+        
+        completionHandler()
     }
 }
